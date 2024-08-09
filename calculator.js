0db61220626691624ed991bd16440cf2c8d3f2f7
akgupta2002D <154490783+akgupta2002D@users.noreply.github.com>
2024-08-09 05:07:53 -0400

    Add functions for four operations and constant variables

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subs(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let number1 = "";
+let number2 = "";
+let operator = "";
+
